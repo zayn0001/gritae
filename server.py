c50fff79f7db9053673a52f871c13dbf504b210c
Mishal <mishal0404@gmail.com>
2024-06-11 00:19:05 +0530

    done

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ def process_urls(company, case_studies, prospect, same_vector_store):
     draft = get_draft(summary)
     return draft
 
-st.title("GritAE Assignment")
+st.title("GritAE Assignment (Not working rn cus there was something wrong with the openai api keys, but will run locally tho with your own key)")
 
 company = st.text_input("Company Website URL", value="www.intercom.com")
 case_studies = st.text_input("Company Case Studies URL", value="www.intercom.com/customers")
